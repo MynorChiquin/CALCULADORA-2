0537b41d3e7b8eaaeec0b614512bb177af30212b
MynorChiquin <ismaelchiquinpop@gmail.com>
2022-02-02 14:05:49 -0600

    implementacion de multiplicacion y division

--- calculadora2.py
+++ calculadora2.py
@@ -1,13 +1,11 @@
-print(" -----------------  CALCULADORA DE PYTHON ------------")
-print("1. operacion suma.")
-print("2. operacion resta")
-print("3. operacion multiplicacion.")
-print("4. operacion division.")
+if o == 3:
+    num1 = int(input("Escriba su primer numero."))
+    num2 = int(input("Escriba su segundo numero."))
+    mult = num1 * num2
+    print("el resultado de la Multiplicacion es: ", mult)
 
-if o == 1:
-    num1 = int(input("Escriba su primer número"))
-    num2 = int(input("Escriba su segundo número"))
-    suma = num1+num2
-    print("El resultado de la suma es: " ,suma)
-else:
-    print("OPCIÓN FUERA DE RANGO")
+if o == 4:
+    num1 = int(input("Escriba su primer numero."))
+    num2 = int(input("Escriba su segundo numero."))
+    div = num1 / num2
+    print("el resultado de la division es: ", mult)
