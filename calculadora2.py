2fca1c45a2dc2058cba8002a5f2180046c88029d
Angel Maaz <angelsantiagomaazcho@gmail.com>
2022-02-02 14:01:37 -0600

    IMPLEMENTACIÓN DE RESTA

--- calculadora2.py
+++ calculadora2.py
@@ -11,5 +11,10 @@ if o == 1:
     num2 = int(input("Escriba su segundo número"))
     suma = num1+num2
     print("El resultado de la suma es: " ,suma)
+elif o == 2:
+    num1 = int(input("Escriba su primer número"))
+    num2 = int(input("Escriba su segundo número"))
+    resta = num1-num2
+    print("El resultado de la suma es: " ,resta)
 else:
     print("OPCIÓN FUERA DE RANGO")
