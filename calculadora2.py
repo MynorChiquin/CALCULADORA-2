a1d396fc19c686488ea89f6b0045aa3461cb7449
MynorChiquin <ismaelchiquinpop@gmail.com>
2022-02-02 13:46:53 -0600

    implementacion de menu principal

---
+++ calculadora2.py
@@ -0,0 +1,7 @@
+print(" -----------------  CALCULADORA DE PYTHON ------------")
+print("1. operacion suma.")
+print("2. operacion resta")
+print("3. operacion multiplicacion.")
+print("4. operacion division.")
+
+o = int(input("Elija una opcion.."))
