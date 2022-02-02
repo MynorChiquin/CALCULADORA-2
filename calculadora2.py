b15dc1c8f95b199fce26b176ba78c8c4be443a96
MynorChiquin <ismaelchiquinpop@gmail.com>
2022-02-02 17:15:57 -0600

    colocacion de seleccion de opciones

--- calculadora2.py
+++ calculadora2.py
@@ -3,6 +3,7 @@ print("1. operacion suma.")
 print("2. operacion resta")
 print("3. operacion multiplicacion.")
 print("4. operacion division.")
+o = int(input("Seleccione una operacion."))
 
 if o == 1:
     num1 = int(input("Escriba su primer número"))
