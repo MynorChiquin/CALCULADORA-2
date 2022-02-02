7327e6088b74bc0ca37b7403e9bfc6638b9eb1b8
MynorChiquin <ismaelchiquinpop@gmail.com>
2022-02-02 17:17:56 -0600

    salto de linea

--- calculadora2.py
+++ calculadora2.py
@@ -15,5 +15,6 @@ elif o == 2:
     num2 = int(input("Escriba su segundo número"))
     resta = num1-num2
     print("El resultado de la suma es: " ,resta)
+
 else:
     print("OPCIÓN FUERA DE RANGO")
