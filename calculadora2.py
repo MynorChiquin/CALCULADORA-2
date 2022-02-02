2bd30f32e2d34d83bd27cb548f30f17278fec05c
Angel Maaz <angelsantiagomaazcho@gmail.com>
2022-02-02 13:46:34 -0600

    IMPLEMENTACIÓN DE SUMA

---
+++ calculadora2.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+if o == 1:
+    num1 = int(input("Escriba su primer número"))
+    num2 = int(input("Escriba su segundo número"))
+    suma = num1+num2
+    print("El resultado de la suma es: " ,suma)
+else:
+    print("OPCIÓN FUERA DE RANGO")
